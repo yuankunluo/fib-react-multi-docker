3e9e681768caccbc3a28a3e6b8f53307659f7e50
yuankunluo <yuankunluo@outlook.com>
2019-10-08 09:52:31 +0800

    copy checkpoint files to src

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -15,9 +15,7 @@ class Fib extends Component {
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({
-      values: values.data
-    });
+    this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
@@ -27,50 +25,52 @@ class Fib extends Component {
     });
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
+
     await axios.post('/api/values', {
-        index: this.state.index
-    })
-    this.setState({
-        index: ''
+      index: this.state.index
     });
+    this.setState({ index: '' });
   };
 
-  renderSeenIndexes(){
-      return this.state.seenIndexes.map(({number})=> number).join(', ');
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
-  renderValues(){
-      const entires = [];
-      for (let key in this.state.values){
-          entires.push(
-              <div key={key}>
-                  For index {key} I calculated {this.state.values[key]}
-              </div>
-          )
-      }
-      return entires;
-  }
+  renderValues() {
+    const entries = [];
 
-  render(){
-      return (
-          <div>
-              <form onSubmit={this.handleSubmit}>
-                  <label>Enter your index</label>
-                  <input 
-                    value={this.state.index} 
-                    onChange={event => this.setState({index:event.target.value})}
-                    />
-                  <button>Submit</button>
-              </form>
-
-              <h3>Indexes I have seen</h3>
-                {this.renderSeenIndexes()}
-              <h3>Calculated Values</h3>
-                {this.renderValues()}
-          </div>
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
       );
+    }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
+      </div>
+    );
   }
 }
 
--- worker/index.js
+++ worker/index.js
@@ -1,27 +1,19 @@
 const keys = require('./keys');
 const redis = require('redis');
 
-
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-
 const sub = redisClient.duplicate();
 
-// function to calculate fib
 function fib(index) {
-    if (index < 2){
-        return 1;
-    } else {
-        return fib(index - 1) + fib( index -2);
-    }
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, message)=>{
-    // insert a index:value to a hash set values
-    redisClient.hset('values', message, fib(parseInt(message)));
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-
 sub.subscribe('insert');
